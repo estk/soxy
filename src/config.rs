2ca8c24037601247eafb9866b4390f4f10557b57
Evan Simmons <esims89@gmail.com>
2018-10-30 19:22:49 -0700

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::net::SocketAddr;
+use toml::Value;
+
+/// Read the `config.toml` at `path`.
+pub fn read_config(path: &str) -> Result<HashMap<String, SocketAddr>, Box<dyn Error>> {
+    let mut input = File::open(path)?;
+    let mut config_content = String::new();
+    input.read_to_string(&mut config_content)?;
+    let config: Value = config_content.parse()?;
+
+    config["domains"]
+        .to_owned()
+        .try_into()
+        .map_err(|e| e.into())
+}
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#![feature(uniform_paths, unboxed_closures, fn_traits)]
+#![warn(unused)]
+mod config;
+use config::read_config;
+use hyper::rt::{self, Future};
+use hyper::service::service_fn;
+use hyper::{Client, Server};
+use log::trace;
+use std::net::SocketAddr;
+
+fn main() {
+    pretty_env_logger::init();
+    let in_addr = ([0, 0, 0, 0], 80).into();
+    let domains = read_config("config.toml").expect("Unable to access config.toml");
+
+    let client_main = Client::new();
+
+    // new_service is run for each connection, creating a 'service'
+    // to handle requests for that specific connection.
+    let new_service = move || {
+        let client = client_main.clone();
+        let dref = domains.clone();
+        // This is the `Service` that will handle the connection.
+        // `service_fn_ok` is a helper to convert a function that
+        // returns a Response into a `Service`.
+        service_fn(move |mut req| {
+            let domain = req
+                .headers()
+                .get("host")
+                .expect("must have host header")
+                .to_str()
+                .expect("must have host header");
+            trace!("sub_domain: {}", domain);
+
+            let sock_addr = dref.get(domain).expect("domain not found in proxy list");
+            trace!("proxying req to {} to {}", domain, sock_addr);
+
+            let uri_string = format!(
+                "http://{}/{}",
+                sock_addr,
+                req.uri().path_and_query().map(|x| x.as_str()).unwrap_or("")
+            );
+            let uri = uri_string.parse().unwrap();
+            *req.uri_mut() = uri;
+            client.request(req)
+        })
+    };
+
+    let server = Server::bind(&in_addr)
+        .serve(new_service)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Listening on http://{}", in_addr);
+
+    rt::run(server);
+}
